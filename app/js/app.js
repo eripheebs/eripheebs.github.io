fac9847968e3628ce67c04cc6f60392b1c4f85e0
eripheebs <erikapheby@gmail.com>
2016-05-19 19:58:52 +0100

    going to use yeoman

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,28 @@
-var eriApp = angular.module('eriApp', ['ngRoute']);
+var eriApp = angular.module('eriApp', ['ngRoute'])
+
+.config(function($routeProvider, $locationProvider) {
+
+    $routeProvider
+        .when('/', {
+            templateUrl : 'partials/home.html',
+            controller : 'MainCtrl'
+        })
+        .when('/welcome', {
+            templateUrl : 'partials/welcome.html',
+            controller : 'MainCtrl'
+        })
+        .when('/portfolio', {
+            templateUrl : 'partials/portfolio.html',
+            controller : 'MainCtrl'
+        })
+        .when('/blog', {
+            templateUrl : 'partials/blog.html',
+            controller : 'MainCtrl'
+        })
+        .when('/cv', {
+            templateUrl : 'partials/cv.html',
+            controller : 'MainCtrl'
+        });
+
+    $locationProvider.html5Mode(true);
+});
